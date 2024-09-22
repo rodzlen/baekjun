04b8bfdaddcc84216a5e3678adb21fc124381082
rodzlen <qkqhrkxdkdy@naver.com>
2024-09-22 22:45:15 +0900

    백준 레퍼지토리 생성

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println("Hello World! "+a);
+        }
+    }
